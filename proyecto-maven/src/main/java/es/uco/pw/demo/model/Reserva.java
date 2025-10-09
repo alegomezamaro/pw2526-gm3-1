d3ae3f43039340bafdd6f5f212b8f7536b280255
MohssinBS <bassatmohssin@gmail.com>
2025-10-09 19:26:27 +0200

    definicion setters y getters clase reserva

--- proyecto-maven/src/main/java/es/uco/pw/demo/model/Reserva.java
+++ proyecto-maven/src/main/java/es/uco/pw/demo/model/Reserva.java
@@ -1,5 +1,42 @@
 package es.uco.pw.demo.model;
 
+import java.time.LocalDate;
+
 public class Reserva {
-  
+
+    private int id;
+    private Embarcacion embarcacion;
+    private String descripcion;
+    private Socio socioSolicitante;
+    private int plazasReserva;
+    private LocalDate fechaReserva;
+    private double precioReserva;
+
+
+    public Reserva(int id,Embarcacion embarcacion,String descripcion,Socio socioSolicitante,int plazasReserva,LocalDate fechaReserva,double precioReserva){
+        this.id = id;
+        this.embarcacion = embarcacion;
+        this.descripcion = descripcion;
+        this.socioSolicitante = socioSolicitante;
+        this.plazasReserva = plazasReserva;
+        this.fechaReserva = fechaReserva;
+        this.precioReserva = precioReserva;
+    }
+
+    public int getId() { return id; }
+    public Embarcacion getEmbarcacion() { return embarcacion; }
+    public String getDescripcion() { return descripcion; }
+    public Socio getSocioSolicitante() { return socioSolicitante; }
+    public int getPlazasReserva() { return plazasReserva; }
+    public LocalDate getFechaReserva() { return fechaReserva; }
+    public double getPrecioReserva() { return precioReserva; }
+
+    public void setId(int id) { this.id = id; }
+    public void setEmbarcacion(Embarcacion embarcacion) { this.embarcacion = embarcacion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+    public void setSocioSolicitante(Socio socioSolicitante) { this.socioSolicitante = socioSolicitante; }
+    public void setPlazasReserva(int plazasReserva) { this.plazasReserva = plazasReserva; }
+    public void setFechaReserva(LocalDate fechaReserva) { this.fechaReserva = fechaReserva; }
+    public void setPrecioReserva(double precioReserva) { this.precioReserva = precioReserva; }
+
 }
